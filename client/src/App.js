6b79273f655f1ea0be5489fec61d62d6de66c0f9
ipruchai <i.pruchai@sportradar.com>
2021-04-27 13:06:56 +0200

    Add h1 tag with text

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fib calculator</h1>
             <p>
               Scroll down ----->
             </p>
